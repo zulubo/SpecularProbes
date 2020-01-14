6a7fa50339cb977e9c0aa8cdad5792b5578fc5c0
zulubo <zach.tsiakalis.brown@gmail.com>
2020-01-14 14:36:09 -0800

    Initial commit

---
+++ Assets/Specular Probes/SpecularProbeLight.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class SpecularProbeLight : MonoBehaviour {
+#if UNITY_EDITOR
+    [Tooltip("The intensity of the Light will be multiplied by this. Recommended to raise for small lights, and lower for large lights")]
+    public float intensityMultiplier = 1;
+    [Tooltip("Radius of the light sphere geometry")]
+    public float radius = 0.25f;
+
+
+    const float intensityConstant = 5;
+
+    private LightRenderer instance;
+    private Light light;
+
+    private void OnDrawGizmosSelected()
+    {
+        Start();
+        Gizmos.color = light.color;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    private class LightRenderer
+    {
+        public GameObject gameObject;
+        public MeshFilter meshFilter;
+        public MeshRenderer renderer;
+        public Transform transform { get { return gameObject.transform; } }
+
+        public LightRenderer(GameObject g)
+        {
+            gameObject = g;
+            meshFilter = g.AddComponent<MeshFilter>();
+            renderer = g.AddComponent<MeshRenderer>();
+        }
+
+        public void Destroy()
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if(light==null)
+            light = GetComponent<Light>();
+    }
+
+    public void Draw()
+    {
+        Start();
+
+        if (instance != null)
+        {
+            Hide();
+        }
+        instance = new LightRenderer(new GameObject("lightRenderer"));
+        instance.transform.localScale = Vector3.one * radius;
+        instance.transform.parent = transform;
+        instance.transform.localPosition = Vector3.zero;
+        instance.meshFilter.sharedMesh = ((GameObject)Resources.Load("SpecSphere", typeof(GameObject))).GetComponent<MeshFilter>().sharedMesh;
+        instance.renderer.sharedMaterial = new Material(Shader.Find("Particles/Standard Unlit"))
+        {
+            color = light.color * CalcBrightness()
+        };
+        instance.gameObject.isStatic = true;
+    }
+
+    public void Hide()
+    {
+        if (instance != null)
+        {
+            if (instance.gameObject != null)
+            {
+                DestroyImmediate(instance.renderer.sharedMaterial);
+                instance.Destroy();
+            }
+        }
+        instance = null;
+    }
+
+    float CalcBrightness()
+    {
+        return light.intensity * intensityMultiplier * intensityConstant;
+    }
+#endif
+}
---
+++ Assets/Specular Probes/SpecularProbeRenderer.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+[RequireComponent(typeof(ReflectionProbe))]
+public class SpecularProbeRenderer : MonoBehaviour {
+
+#if UNITY_EDITOR
+    [Tooltip("Will only draw specular highlights for lights in this radius")]
+    public float radius = 100;
+
+    private ReflectionProbe probe;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 1, 1, 0.25f);
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    private void Start()
+    {
+        if (Application.isPlaying) Destroy(this);
+    }
+
+    private void OnEnable()
+    {
+        UnityEditor.Lightmapping.bakeCompleted += OnBakeCompleted;
+    }
+
+    private void OnDisable()
+    {
+        UnityEditor.Lightmapping.bakeCompleted -= OnBakeCompleted;
+    }
+
+    void OnBakeCompleted()
+    {
+        Debug.Log("Baking Specular Highlights");
+        Render();
+    }
+
+    [ContextMenu("Render")]
+    public void Render()
+    {
+        probe = GetComponent<ReflectionProbe>();
+
+        SpecularProbeLight[] allLights = FindObjectsOfType<SpecularProbeLight>();
+        List<SpecularProbeLight> closeLights = new List<SpecularProbeLight>();
+
+        for (int i = 0; i < allLights.Length; i++)
+        {
+            //find lights within radius of probe
+            if ((allLights[i].transform.position - transform.position).sqrMagnitude < radius * radius)
+            {
+                closeLights.Add(allLights[i]);
+                // create specular highlight sphere
+                allLights[i].Draw();
+            }
+        }
+        //render probe
+        string path = UnityEditor.AssetDatabase.GetAssetPath(probe.bakedTexture);
+        UnityEditor.Lightmapping.BakeReflectionProbe(probe, path);
+
+        // remove all created lights, cleaning up scene
+        for (int i = 0; i < closeLights.Count; i++)
+        {
+            closeLights[i].Hide();
+        }
+    }
+
+    [ContextMenu("Render All")]
+    public void RenderAll()
+    {
+        foreach(SpecularProbeRenderer r in FindObjectsOfType<SpecularProbeRenderer>())
+        {
+            r.Render();
+        }
+    }
+
+#endif
+}
